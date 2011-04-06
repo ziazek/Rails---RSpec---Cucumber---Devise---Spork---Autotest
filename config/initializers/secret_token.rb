03699715a88bdf65d97b2113b2132bd71380002b
Zek <ziazek@gmail.com>
2011-04-06 14:17:06 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DaytumB::Application.config.secret_token = '6ab52327dfbeecd4e5ba221967bfe24d0fa8e65b3ebd774f43dcee2b4e293fbdfc03a39d69fcb5ec97cbdaa787d13d9bfeb0de0ba4967b1595ade520c7cdce80'
